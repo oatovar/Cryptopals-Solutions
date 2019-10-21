b790c6ba590b09f5066669bc14b453313cce474d
Oscar Alberto Tovar <oscar.alberto.tovar@gmail.com>
2019-10-20 23:45:43 -0500

    Add S02 Challenge 10 Solution

---
+++ S02/c10.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from base64 import b64decode
+from Crypto.Cipher import AES
+from c9 import pkcs7_padding
+
+def decrypt_aes_cbc(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
+    '''
+    Encrypts a given cleartext using the given key, iv, and block size
+
+    returns ciphertext bytes
+    '''
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return cipher.decrypt(ciphertext)
+
+def encrypt_aes_cbc(cleartext: bytes, key: bytes, iv: bytes) -> bytes:
+    '''
+    Encrypts a given cleartext using the given key, iv, and block size
+
+    returns ciphertext bytes
+    '''
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return cipher.encrypt(pkcs7_padding(cleartext, AES.block_size))
+
+if __name__ == '__main__':
+    from pathlib import Path
+    filepath = str(Path(__file__).parent.absolute()) + '/' + 'c10_input.txt'
+    file = open(filepath, 'r')
+    cleartext = b64decode(file.read())
+    file.close()
+    print(decrypt_aes_cbc(cleartext, b'YELLOW SUBMARINE', bytes(AES.block_size)).decode())
