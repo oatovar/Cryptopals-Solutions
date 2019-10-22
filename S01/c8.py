5aba9a2717631502b71bcfea7316ada75a18273b
Oscar Alberto Tovar <oscar.alberto.tovar@gmail.com>
2019-10-21 21:12:51 -0500

    Refactor detect_aes_ecb

--- S01/c8.py
+++ S01/c8.py
@@ -2,6 +2,14 @@
 
 from Crypto.Cipher import AES
 
+def duplicate_blocks(ciphertext: bytes) -> int:
+    '''
+    Returns the number of duplicate blocks within the ciphertext bytes
+    '''
+    blocks = [ciphertext[i:i + AES.block_size] for i in range(0, len(ciphertext), AES.block_size)]
+    block_count = len(blocks)
+    return (block_count - len(set(blocks)))
+
 def detect_aes_ecb(filepath: str) -> list:
     '''
     Generates a list of candidates that are highly likely to be
@@ -17,9 +25,7 @@ def detect_aes_ecb(filepath: str) -> list:
             b = bytes.fromhex(line.strip())
             ciphertexts.append(b)
     for _, c in enumerate(ciphertexts):
-        blocks = [c[i:i + AES.block_size] for i in range(0, len(c), AES.block_size)]
-        block_count = len(blocks)
-        duplicate_count = block_count - len(set(blocks))
+        duplicate_count = duplicate_blocks(c)
         if duplicate_count > 0:
             tmp = (c.hex(), duplicate_count)
             candidates.append(tmp)
